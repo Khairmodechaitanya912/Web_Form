85e3693bf981ff4bcfbb245b3b263716cca59141
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-03-17 23:06:33 +0530

    Simple Web Login Form

---
+++ Login Page Form/JS/script.js
@@ -0,0 +1,6 @@
+function getdata(){
+    const Username = document.getElementById("Uname").value;
+    const Password = document.getElementById("Pass").value;
+    alert(`Username :- ${Username}
+Password :- ${Password}`)
+}
