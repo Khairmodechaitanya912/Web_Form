ee2a200261ec69de429730bb3c5b19a98c6d5ed2
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-03-26 00:31:29 +0530

    Web Form Brand Add update

--- Brand Add/JS/script.js
+++
@@ -1,9 +0,0 @@
-function getdata(){
-    const Brand_ID = document.getElementById("ID").value;
-    const Brand_Name = document.getElementById("Name").value;
-    const Brand_Status = document.getElementById("Use").value;
-
-    alert(`Brand Id :- ${Brand_ID}
-Brand Name :- ${Brand_Name}
-Status :- ${Brand_Status}`);
-}
